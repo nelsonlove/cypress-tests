8b9c6a3b1904ee1269f0d33ab7271143462d299a
nelsonlove <nelson.love@me.com>
2021-10-27 16:42:23 -0400

    Revise infinite scroll test

--- cypress/integration/3-infinite-scroll.spec.js
+++ cypress/integration/3-infinite-scroll.spec.js
@@ -1,21 +1,45 @@
+/// <reference types="cypress" />
+
 // 3. Another feature used widely in ESP is infinite scroll. http://the- internet.herokuapp.com/infinite_scroll
 // contains a div with infinite scroll. Please automate a test that assert that as you scroll the div, new child
 // divs populate the DOM
 
-it('New child divs are rendered on scroll', () => {
-  cy.visit('http://the-internet.herokuapp.com/infinite_scroll');
+describe('Infinite scroll', () => {
 
-  // Initally there are two divs
-  const initial_divs = 2
-  cy.get('.jscroll-added')
-    .should('have.lengthOf', initial_divs)
+  it('New child divs are rendered on scroll', () => {
+    cy.visit('http://the-internet.herokuapp.com/infinite_scroll');
 
-  // Scrolling five times should yield five new divs
-  for (let i = initial_divs + 1; i < initial_divs + 5; i++) {
-    // Wait appears necessary for div to render
-    cy.scrollTo('bottom')
-      .wait(100)
-    cy.get('.jscroll-added')
-      .should('have.lengthOf', i)
-  }
+    let initial_divs;
+
+    cy.get('div.jscroll-inner')
+      .children()
+      .its('length')
+      .then((length) => {
+        initial_divs = length;
+      });
+
+    // Scrolling five times should yield five new divs
+    for (let i = initial_divs; i < initial_divs + 5; i++) {
+      cy.get('div.jscroll-inner')
+        .children()
+        .should('have.lengthOf', i);
+
+      cy.scrollTo('bottom')
+        .wait(500);
+    }
+  });
 });
+// for (let i = 0; i < 5; i++) {
+//   cy.get('div.jscroll-inner')
+//     .children()
+//     .should('have.lengthOf', i)
+//     .its('length')
+//     .then(children => {
+//       cy.wrap(children)
+//         .its('length')
+//         .should('eq', numberOfChildren)
+//     })
+//   cy.scrollTo('bottom', {duration: 5000})
+//     .wait('@blog')
+//     .then(() => numberOfChildren += 4)
+// }
